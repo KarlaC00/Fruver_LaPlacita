9f8659be2259f388b95a0f71e57362315aa6d9cc
Karla Valentina Cuellar Perdomo <karvalentinap@gmail.com>
2025-04-27 14:30:24 -0500

    Beta_nav_footer

--- fruver/src/App.jsx
+++ fruver/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/nav"; // Asegúrate que el nombre y ruta sean correctos
+import Footer from "./components/footer"; 
 
 function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
